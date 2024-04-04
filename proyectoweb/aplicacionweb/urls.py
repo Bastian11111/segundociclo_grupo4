6fc4ce83b6ebc817a6245698142d77fbc31d806b
Bastián Rabelo <133687016+Bastian11111@users.noreply.github.com>
2024-04-04 02:38:10 -0300

    importacion de enlaces .html
    Se agregaron los archivos HTML a la lógica de ingreso de urls.py y views.py de la carpeta "aplicacionweb".
    
    Nota: No se respetaron las mayúsculas en los nombres.

--- proyectoweb/aplicacionweb/urls.py
+++ proyectoweb/aplicacionweb/urls.py
@@ -1,9 +1,39 @@
 #CON ESTE SOLO VEMOS HOME
 
 from django.urls import path #Lo primero que hacemos es importar el path
-from .views import home #Lo segundo es importar "home" por ahora solo este, aunque se pueden importar más vistas
+from .views import principal #Lo segundo es importar "home" por ahora solo este, aunque se pueden importar más vistas
+from .views import recuperarcontra
+from .views import registrarse
+from .views import micuentatf
+from .views import lugares
+from .views import logros
+from .views import iniciosesion
+from .views import historia
+from .views import forowiki
+from .views import flora
+from .views import enemigos
+from .views import consumibles
+from .views import construcciones
+from .views import armas
+from .views import animales
 
 #LLAMA A LAS VISTAS UNA POR UNA (EN ESTE CASO LA PRIMERA))
 urlpatterns = [
-    path('', home, name='home'), #La primera vista que se llama es home, lo vacio dice que es la vista principal, home es la vista que se llama y el otro home es un alias 
+    path('', principal, name='Principal'), #La primera vista que se llama es home, lo vacio dice que es la vista principal, home es la vista que se llama y el otro home es un alias 
+    path('recuperarcontra/', recuperarcontra, name='rcontra'),
+    path('registrarse/', registrarse, name='registrarse'),
+    path('micuentatf/', micuentatf, name='micuenta'),
+    path('lugares/', lugares, name='lugares'),
+    path('logros/', logros, name='logros'),
+    path('iniciosesion/', iniciosesion, name='iniciosesion'),
+    path('historia/', historia, name='historia'),
+    path('forowiki/', forowiki, name='forowiki'),
+    path('flora/', flora, name='flora'),
+    path('enemigos/', enemigos, name='enemigos'),
+    path('consumibles/', consumibles, name='consumibles'),
+    path('construcciones/', construcciones, name='construcciones'),
+    path('armas/', armas, name='armas'),
+    path('animales/', animales, name='animales'),
+    
+    
 ]
--- proyectoweb/aplicacionweb/views.py
+++ proyectoweb/aplicacionweb/views.py
@@ -1,5 +1,51 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'aplicacionweb/Animales.html') #El render lo que hace es renderizar la vista, el request es lo que se le envia a la vista, y el nombre de la vista que se va a renderizar
+def principal(request):
+    return render(request, 'aplicacionweb/menuprincipal_wiki.html') #El render lo que hace es renderizar la vista, el request es lo que se le envia a la vista, y el nombre de la vista que se va a renderizar
+
+def recuperarcontra(request):
+    return render(request, 'aplicacionweb/recuperarcontra.html')
+
+def registrarse(request):
+    return render(request, 'aplicacionweb/registrase_wiki.html')
+
+def micuentatf(request):
+    return render(request, 'aplicacionweb/micuentatf.html')
+
+def lugares(request):
+    return render(request, 'aplicacionweb/lugarestf.html')
+
+def logros(request):
+    return render(request, 'aplicacionweb/logros.html')
+
+def iniciosesion(request):
+    return render(request, 'aplicacionweb/inicio_sesion_wiki.html')
+
+def historia(request):
+    return render(request, 'aplicacionweb/historia.html')
+
+def forowiki(request):
+    return render(request, 'aplicacionweb/forowiki.html')
+
+def flora(request):
+    return render(request, 'aplicacionweb/flora.html')
+
+def enemigos(request):
+    return render(request, 'aplicacionweb/enemigos.html')
+
+def consumibles(request):
+    return render(request, 'aplicacionweb/consumibles.html')
+
+def construcciones(request):
+    return render(request, 'aplicacionweb/construcciones.html')
+
+def armas(request):
+    return render(request, 'aplicacionweb/armas.html')
+
+def animales(request):
+    return render(request, 'aplicacionweb/animales.html')
+
+
+
+
